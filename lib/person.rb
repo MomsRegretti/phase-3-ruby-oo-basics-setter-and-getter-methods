00e4e396f8c111a0438a6b51d89730a9603d25f8
CharlieRojas <crc_185@yahoo.com>
2022-05-04 21:02:54 -0500

    commit tree 26fe2ef36898458ce36cc19dd6a00a822aa0b6e9

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,23 @@
 class Person
 
+    # setter method
+    def name=(name)
+    @name = name
+    end
+
+    # getter method
+    def name
+    @name
+    end
+
+    def job=(job)
+        @job = job
+    end
+
+    def job
+        @job
+    end
+    
+    kanye= Person.new
+    kanye.name = "Kanye"
 end
